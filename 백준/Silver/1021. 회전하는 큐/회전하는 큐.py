fca5cbd598a3088691d2b466945c809580cd337d
jeongmin-bak <pjm9673@gmail.com>
2023-11-20 19:28:39 +0900

    [Silver III] Title: 회전하는 큐, Time: 68 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,25 +1,27 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
-idxs = list(map(int, input().split()))
-dq = deque([x for x in range(1,n+1)])
-re = [x for x in range(1, n+1)]
-count = 0
+idxs= list(map(int, input().split()))
+
+dq = deque([i for i in range(1, n+1)])
+re = list(dq)
+cnt = 0
 
 for idx in idxs:
     while True:
-        if dq[0] == idx:
+        if idx == dq[0]:
             dq.popleft()
             break
-        else:
-            if dq.index(idx) < len(dq)/2:
-                while dq[0] != idx:
-                    dq.append(dq.popleft())
-                    count += 1
-            else:
-                while dq[0] != idx:
-                    dq.appendleft(dq.pop())
-                    count += 1
 
-print(count)
+        elif dq.index(idx) < len(dq)/2:
+            while dq[0] != idx:
+                dq.append(dq.popleft())
+                cnt += 1
+        else:
+            while dq[0] != idx:
+                dq.appendleft(dq.pop())
+                cnt += 1
+
+print(cnt)
