3057be21b152a51a1cdb2f621db742a9e978616f
jeongmin-bak <pjm9673@gmail.com>
2024-01-23 17:25:41 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,19 +1,19 @@
+import sys
 
-n = int(input())
+input = sys.stdin.readline
 
-for case in range(n):
-    data = input()
-    stack = []
+T = int(input())
+for case in range(T):
     flag = True
-    for val in data:
-        if val == "(":
-            stack.append(val)
-        elif val == ")" and stack:
+    stack = []
+    for s in input():
+        if s == "(":
+            stack.append(s)
+        elif s == ")" and stack:
             stack.pop()
-        elif val == ")" and len(stack) == 0:
+        elif s == ")" and len(stack) == 0:
             flag = False
             break
-
     if flag == False or stack:
         print("NO")
     else:
