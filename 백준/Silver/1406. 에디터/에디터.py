e02904e3fd44a41f5a4e61592e0fb52f51247670
jeongmin-bak <pjm9673@gmail.com>
2024-02-06 14:54:13 +0900

    [Silver II] Title: 에디터, Time: 332 ms, Memory: 159600 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,18 +1,27 @@
 import sys
+from collections import deque
+
 input = sys.stdin.readline
-st1 = list(input().rstrip())
+stack1 = deque(input().rstrip())
 M = int(input())
-st2 = list()
+cursor = len(stack1)
+stack2 = deque()
 for _ in range(M):
     command = list(map(str, input().split()))
     if command[0] == 'P':
-        st1.append(command[1])
-    elif command[0] == 'L' and st1:
-        st2.append(st1.pop())
-    elif command[0] == 'D' and st2:
-        st1.append(st2.pop())
-    elif command[0] == 'B' and st1:
-        st1.pop()
+        if cursor == len(stack1):
+            stack1.append(command[1])
+            cursor += 1
+    elif command[0] == "L":
+        if cursor > 0:
+            stack2.appendleft(stack1.pop())
+            cursor -= 1
+    elif command[0] == "D" and stack2:
+        stack1.append(stack2.popleft())
+        cursor += 1
+    elif command[0] == "B":
+        if cursor > 0:
+            stack1.pop()
+            cursor -= 1
 
-st1.extend(reversed(st2))
-print(''.join(st1))
+print(''.join(stack1) + ''.join(stack2))
