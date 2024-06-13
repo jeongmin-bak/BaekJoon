4314430713900a87e61f5e8dffc756fca450fd03
jeongmin-bak <pjm9673@gmail.com>
2024-06-13 19:49:57 +0900

    [Silver II] Title: 아기 상어 2, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.py
@@ -1,45 +1,29 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
-n, m = map(int, input().strip().split())
+n, m = tuple(map(int, input().split()))
+shark_list = [list(map(int, input().split())) for _ in range(n)]
 
-arr = [list(map(int, input().split())) for _ in range(n)]
-
-
-# 인접 방향(대각선 포함)
-d = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
-
-
-def bfs(y, x):
-    q = deque()
-    q.append((y, x))
-    visited = [[0] * m for _ in range(n)]
-    isShark = 0  # flag
-    while q:
-        y, x = q.popleft()
-        for dy, dx in d:
-            Y = y + dy
-            X = x + dx
-            if 0 <= X < m and 0 <= Y < n and visited[Y][X] == 0:
-                if arr[Y][X] == 0:
-                    q.append((Y, X))
-                    visited[Y][X] = visited[y][x] + 1
-                else:
-                    ans = visited[y][x] + 1
-                    isShark = 1
-        if isShark:
-            break
-
-    return ans
-
+q = deque()
+for i in range(n):
+    for j in range(m):
+        if shark_list[i][j] == 1:
+            q.append([i, j])
 
 ans = 0
-for y in range(n):
-    for x in range(m):
-        if arr[y][x] != 1:
-            if ans < bfs(y, x):
-                ans = bfs(y, x)
+dx = [-1, -1, -1, 0, 1, 0, 1, 1]
+dy = [-1, 0, 1, 1, 1, -1, 0, -1]
 
-print(ans)
+while q:
+    x, y = q.popleft()
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if shark_list[nx][ny] == 0:
+                q.append([nx, ny])
+                shark_list[nx][ny] = shark_list[x][y] + 1
+                ans = max(ans, shark_list[x][y] + 1)
+
+print(ans-1)
