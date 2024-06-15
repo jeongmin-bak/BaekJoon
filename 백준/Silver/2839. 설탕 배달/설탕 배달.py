fddfc60f90c781799393febc122ecb9fbcc4e2a9
jeongmin-bak <pjm9673@gmail.com>
2024-06-15 18:35:32 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+cnt = 0
+if n % 5 == 0:
+    print(n//5)
+else:
+    p = 0
+    while n > 0:
+        n -= 3
+        cnt += 1
+        if n % 5 == 0:
+            cnt += n // 5
+            print(cnt)
+            break
+        elif n == 1 or n == 2:
+            print(-1)
+            break
+        elif n == 0:
+            print(cnt)
+            break
