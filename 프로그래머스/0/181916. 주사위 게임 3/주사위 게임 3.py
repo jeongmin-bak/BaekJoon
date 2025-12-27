69469157af9afa84938e28c378dad12bb708a650
jeongmin-bak <pjm9673@gmail.com>
2025-12-27 19:26:54 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -0,0 +1,24 @@
+from collections import Counter
+def solution(a, b, c, d):
+
+    answer = 0
+
+    li = [a,b,c,d]
+    cnt_li = Counter(li)
+    cnt_tuple = cnt_li.most_common()
+
+    keys = list(cnt_li.keys())
+    values = list(cnt_li.values())
+    if(len(keys) == 1): # keys값이 1개이면 Count가 4일것
+        answer = keys[0] * 1111
+    elif(len(keys) == 2):
+        if values[0] == values[1]:
+            answer = (keys[0] + keys[1]) * abs(keys[0]-keys[1])
+        else:
+            answer = (10 * cnt_tuple[0][0] + cnt_tuple[1][0]) ** 2
+    elif(len(keys) == 4):
+        answer = min(li)
+    else:
+        answer = cnt_tuple[1][0] * cnt_tuple[2][0]
+
+    return answer
